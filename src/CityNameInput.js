31912dbd2d58f992e4254b7dc7fd163fb158d91b
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-02 14:20:23 -0700

    left in an extra input in city name js

--- src/CityNameInput.js
+++ src/CityNameInput.js
@@ -7,7 +7,6 @@ export default function CityNameInput(props) {
       {/* this component should have a prop called setCityName, which is a function */}
       <input onChange={(e) => props.setCityName(e.target.value)} />
       {/* on change, this component should use e.target.value to pass the correct id to the setCityName function */}
-      <input />
     </section>
   );
 }
