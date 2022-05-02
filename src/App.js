956ec8585a1418297044323472d7cf688ecc82ec
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-02 12:18:08 -0700

    fixed app test and started declaring state in app js  and working on castle dropdown

--- src/App.js
+++ src/App.js
@@ -11,8 +11,13 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineId, setSkyline] = useState(1);
+  const [waterfrontId, setWaterfront] = useState(1);
+  const [castleId, setCastle] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
-  // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [cityName, setCityName] = useState('Portland');
+  // finally, you'll need an array of slogans, which could start out as ['The City of Excellents'] for example
+  const [slogans, setSlogans] = useState(['The City of Excellence']);
 
   return (
     <div className="App">
